541ca13ab407ee23d35d14a286c4f511fb115987
ulas742 <ulas.742@gmail.com>
2017-05-15 20:26:14 +0300

    Minor fix
    Minor fix

--- UlascanErsoyEHN105HW4.c
+++ UlascanErsoyEHN105HW4.c
@@ -9,11 +9,11 @@ void printM(int* sizex , int* sizey , int A[*sizex][*sizey] , int B[*sizex][*siz
 
 int main(){
 
-	int _sizex = 5 , _sizey = 4 ;			   //Declare
+	int _sizex = 5 , _sizey = 4 , i;		   //Declare
 	
 	int A[_sizex][_sizey] , B[_sizex][_sizey]; //Declare
 
-	for(int i = 0 ; i < 5 * 4 ; i++){
+	for(i = 0 ; i < 5 * 4 ; i++){
 	       	
 		A[i%5][i/5] = ("\n %d" , ((i * 2) + 1) + 3 );	//Initialize A
 		B[i%5][i/5] = 0;								//Initialize B 
@@ -26,13 +26,14 @@ int main(){
 
 void calculate(int* sizex , int* sizey , int A[*sizex][*sizey] , int B[*sizex][*sizey]){
 
+	int i , k; 
 	
-	for(int i = 0 ;  i < (*sizex) * (*sizey) ; i++){
+	for(i = 0 ;  i < (*sizex) * (*sizey) ; i++){
 
 			printf("\n%d : " , A[i%5][i/5]);
 
 			int curr = A[i%5][i/5];
-			for(int k = 2 ; curr!= 1 ;){
+			for(k = 2 ; curr!= 1 ;){
 
 			
 			if(curr % k == 0){
@@ -63,8 +64,8 @@ void printM(int* sizex , int* sizey , int A[*sizex][*sizey] , int B[*sizex][*siz
 
 
 	printf("A : \t\t\t\t\t B : \n\n");
-
-	for(int i = 0 ;  i < (*sizey) ; i++){
+	int i ;
+	for(i = 0 ;  i < (*sizey) ; i++){
 
 		printf("%d\t%d\t%d\t%d\t%d\t|\t%d\t%d\t%d\t%d\t%d\n" , A[0][i] , A[1][i], A[2][i] , A[3][i]  , A[4][i],
 													   		   B[0][i] , B[1][i], B[2][i] , B[3][i]  , B[4][i]);
